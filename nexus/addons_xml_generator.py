5604e5dd8db0beff7b371173101803cb715574a9
dregs1 <black1977eagle@gmail.com>
2024-06-05 19:00:13 +0100

    ok

--- nexus/addons_xml_generator.py
+++ nexus/addons_xml_generator.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # *
 # *  Copyright (C) 2012-2013 Garrett Brown
 # *  Copyright (C) 2010      j48antialias
@@ -19,13 +20,16 @@
 # *
 # *  Based on code by j48antialias:
 # *  https://anarchintosh-projects.googlecode.com/files/addons_xml_generator.py
- 
+
 """ addons.xml generator """
- 
+
 import os
 import sys
-import hashlib
- 
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+addonsxml_file = os.path.join(dir_path, "addons.xml")
+addons_md5 = os.path.join(dir_path, "addons.xml.md5")
+
 # Compatibility with 3.0, 3.1 and 3.2 not supporting u"" literals
 if sys.version < '3':
     import codecs
@@ -34,111 +38,82 @@ if sys.version < '3':
 else:
     def u(x):
         return x
- 
+
 class Generator:
     """
         Generates a new addons.xml file from each addons addon.xml file
         and a new addons.xml.md5 hash file. Must be run from the root of
         the checked-out repo. Only handles single depth folder structure.
     """
-    def __init__( self ):
+    def __init__(self):
         # generate files
         self._generate_addons_file()
         self._generate_md5_file()
         # notify user
         print("Finished updating addons xml and md5 files")
- 
-    def _generate_addons_file( self ):
+    
+    def _generate_addons_file(self):
         # addon list
-        addons = os.listdir( "." )
+        #addons = os.listdir( "." )
+        addons = os.listdir(dir_path)
         # final addons text
         addons_xml = u("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<addons>\n")
         # loop thru and add each addons addon.xml file
         for addon in addons:
+            addon = os.path.join(dir_path, addon)
             try:
                 # skip any file or .svn folder or .git folder
-                if ( not os.path.isdir( addon ) or addon == ".svn" or addon == ".git" ): continue
+                if (not os.path.isdir(addon) or addon.endswith(".svn") or addon.endswith(".git")): continue
                 # create path
-                _path = os.path.join( addon, "addon.xml" )
+                _path = os.path.join(addon, "addon.xml")
                 # split lines for stripping
-                xml_lines = open( _path, "r" , encoding="UTF-8").read().splitlines()
+                xml_lines = open(_path, "r" ).read().splitlines()
                 # new addon
                 addon_xml = ""
                 # loop thru cleaning each line
                 for line in xml_lines:
                     # skip encoding format line
-                    if ( line.find( "<?xml" ) >= 0 ): continue
+                    if (line.find("<?xml") >= 0): continue
                     # add line
                     if sys.version < '3':
-                        addon_xml += unicode( line.rstrip() + "\n", "UTF-8" )
+                        addon_xml += unicode(line.rstrip() + "\n", "UTF-8")
                     else:
                         addon_xml += line.rstrip() + "\n"
                 # we succeeded so add to our final addons.xml text
                 addons_xml += addon_xml.rstrip() + "\n\n"
             except Exception as e:
                 # missing or poorly formatted addon.xml
-                print("Excluding %s for %s" % ( _path, e ))
+                print("Excluding %s for %s" % (_path, e))
         # clean and add closing tag
         addons_xml = addons_xml.strip() + u("\n</addons>\n")
         # save file
-        self._save_file( addons_xml.encode( "UTF-8" ), file="addons.xml" )
- 
-    def _generate_md5_file( self ):
+        self._save_file(addons_xml.encode( "UTF-8" ), file=addonsxml_file)
+    
+    def _generate_md5_file(self):
         # create a new md5 hash
         try:
             import md5
-            m = md5.new( open( "addons.xml", "r" ).read() ).hexdigest()
+            m = md5.new(open(addonsxml_file, "r").read()).hexdigest()
         except ImportError:
             import hashlib
-            m = hashlib.md5( open( "addons.xml", "r", encoding="UTF-8" ).read().encode( "UTF-8" ) ).hexdigest()
- 
+            m = hashlib.md5(open(addonsxml_file, "r", encoding="UTF-8").read().encode("UTF-8") ).hexdigest()
+        
         # save file
         try:
-            self._save_file( m.encode( "UTF-8" ), file="addons.xml.md5" )
+            self._save_file( m.encode( "UTF-8" ), file=addons_md5)
         except Exception as e:
             # oops
-            print("An error occurred creating addons.xml.md5 file!\n%s" % e)
- 
-    def _save_file( self, data, file ):
+            print("An error occurred creating addons.xml.md5 file!\n%s" %e)
+    
+    def _save_file(self, data, file):
         try:
             # write data to the file (use b for Python 3)
-            open( file, "wb" ).write( data )
+            open(file, "wb").write(data)
         except Exception as e:
             # oops
-            print("An error occurred saving %s file!\n%s" % ( file, e ))
- 
- 
+            print("An error occurred saving %s file!\n%s"%(file, e))
+
+
 if ( __name__ == "__main__" ):
     # start
     Generator()
-	
-# parent_folder = './'
-# all_dirs = []
-# for dir_name in os.listdir(parent_folder):
-#     subdir = os.path.join(parent_folder, dir_name)
-#     if os.path.isdir(subdir) and dir_name not in [".git", "externals"]:
-#         # addon directory
-#         all_dirs.append(dir_name)
-#         html = "<html>\n<body>\n<h1>Directory listing for %s</h1>\n<hr/>\n<pre>" % dir_name
-#         html += "<a href=\"../index.html\">..</a>\n"
-#         for filename in os.listdir(subdir):
-#             if filename.endswith(".zip"):
-#                 file = os.path.join(subdir, filename)
-#                 # append zip to html listing
-#                 html += "<a href=\"%s\">%s</a>\n" % (filename, filename)
-#         html += "</pre>\n</body>\n</html>"
-#         html_file = os.path.join(subdir, "index.html")
-#         print ("write html file %s" % html_file)
-#         with open(html_file, 'w') as f:
-#             f.write(html)
-
-# # write main index.html
-# html = "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
-# for dir_name in all_dirs:
-#     dir_path = os.path.join(parent_folder, dir_name, 'index.html')
-#     html += "<a href=\"%s\">%s</a>\n" % (dir_path, dir_name)
-# html += "</pre>\n</body>\n</html>"
-# html_file = os.path.join(parent_folder, "index.html")
-# print ("write html file %s" % html_file)
-# with open(html_file, 'w') as f:
-#     f.write(html)
